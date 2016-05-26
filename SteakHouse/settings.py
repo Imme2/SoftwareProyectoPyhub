6123342fa3e45146497d1187b38f56edcc76b721
Elechero <davidhervil@gmail.com>
2016-05-25 22:56:08 -0400

    Adicion de pagina de inicio. Link de menu drop-down a registro proveedor,link a log in. Instalacion de aplicacion de inicio,Menu y Perfil. (Modificacion en el settings.py de SteaKHouse)

--- SteakHouse/settings.py
+++ SteakHouse/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Registro',
+    'inicio',
+    'Menu',
+    'Perfil',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
