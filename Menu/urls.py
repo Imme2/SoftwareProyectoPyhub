170bdd37ea36a50f6778d00482be7e4c5d9310ba
Gabriel Gimenez <12-11006@usb.ve>
2016-06-03 12:19:47 -0430

    Arreglado base de datos y limpiada

--- Menu/urls.py
+++ Menu/urls.py
@@ -4,6 +4,7 @@ from Menu import views
 urlpatterns = [
     url(r'^editar/$',views.editarMenu),
     url(r'^editar/(?P<idMenu>[A-Za-z0-9]+)/$',views.editarMenu),
-    url(r'^crear/$', views.crearMenu)
+    url(r'^crear/$', views.crearMenu),
+    url(r'^crearIngrediente/$', views.crearIngrediente)
 ]
 
