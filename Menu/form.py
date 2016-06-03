2c83944ed3ef291707c0494ba28a6019d4586d07
Gabriel Gimenez <12-11006@usb.ve>
2016-06-03 10:32:36 -0430

    Eliminado crear menu innecesario, y redireccionar a editar si estas logeado

--- Menu/form.py
+++ Menu/form.py
@@ -6,36 +6,6 @@ from django.contrib.auth.models import User, Permission
 from Registro.models import perfil, proveedor, menu,item,contiene
 import datetime
 
-class formMenuCrear(forms.Form):
-
-    idMenu = forms.DecimalField(min_value = 0, label = "Id del menu")
-    nombreMenu = forms.CharField(max_length= 50, label = "Nombre del Menu")
-
-    def clean(self):
-        cleaned_data = super(formMenuCrear, self).clean()
-        # idMenu = cleaned_data.get('idMenu')
-        nombreMenu = cleaned_data.get('nombreMenu')
-
-        if menu.objects.filter(nombre = nombreMenu).exists():
-            msg = "El nombre del menu ya existe"
-            self.add_error('nombreMenu', msg)
-            
-        if menu.objects.filter(idMenu = idMenu).exists():
-            msg = "El id del menu ya existe"
-            self.add_error('idMenu', msg)
-
-        return cleaned_data
-
-    def save(self):
-        idMenu = self.cleaned_data['username']
-        nombreMenu = self.cleaned_data['nombre']
-        menuEntry = menu.objects.create(idMenu = idMenu)
-        menuEntry.nombre = nombreMenu
-        menuEntry.save()
-        return menuEntry
-
-
-
 class menuSelector(forms.Form):
 
     listaMenus = menu.objects.all()
@@ -44,7 +14,7 @@ class menuSelector(forms.Form):
     
 class formPlatoSelector(forms.Form):
 
-    nombrePlato = forms.CharField(label = "Nombre del plato")
+    nombrePlato = forms.CharField(label = "Nombre de Menu:")
     platos = forms.ModelMultipleChoiceField(item.objects.all(), required=True, widget=forms.CheckboxSelectMultiple(), label='Selecciona los objetos del menu')
 
     def __init__(self, menuId = None, *args, **kwargs):
