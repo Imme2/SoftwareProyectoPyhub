e59da097617aaa1ad462a4ffc577ae3706955ded
Erick <ericksilvarodriguez@gmail.com>
2016-06-16 18:39:36 -0400

    creado formset para inicio de platos

---
+++ inicio/form.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User, Permission
+from Registro.models import ingrediente, perfil, proveedor, menu, item,contiene,posee
+
+
+
+class formMostrarPlato(forms.ModelForm):
+
+	cantidad = forms.IntegerField(min_value = 0,required = True, initial = 0)
+
+	class Meta:
+		model = item
+		exclude = ('idItem','tipo','poseeRel','foto')
+
+'''
+class formMostrarMenu(forms.Form):
+
+    platos = forms.ModelMultipleChoiceField(contiene.objects.filter(idMenu = self.menuId), required=True, widget=forms.CheckboxSelectMultiple(attrs={'type':"checkbox",}), label='Selecciona los objetos del menu')
+
+    def __init__(self, menuId = None, *args, **kwargs):
+    	self.menuId = kwargs.pop('menuId', None)
+        super(formMenu, self).__init__(*args, **kwargs)
+
+    def save(self, menuId):
+
+        for x in agregar:
+            contiene.objects.create(idMenu = menuObj, idItem = x)
+        for x in remover:
+            contiene.objects.filter(idMenu = menuObj, idItem = x).delete()
+        return None
+
+    def create(self):
+        return menu.objects.create(nombre = self.cleaned_data['nombreMenu']).idMenu
+'''
--- inicio/views.py
+++ inicio/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from inicio.controlador import getCurrentMenu
+from inicio.form import formMostrarPlato
+from django.forms import modelformset_factory
+from Registro.models import parametro,item
+
+
 
 def index(request):
-    menu = getCurrentMenu()
-    return render(request,'inicio/home.html',{'menu': menu})
+    parametros = parametro.objects.all()
+    parametrosActuales = parametros[0]
+    menuAct = parametrosActuales.menuActual
+    platos = menuAct.contieneRel.all()
+
+
+    formSetPlatos = modelformset_factory(item, form = formMostrarPlato,extra = 0)
+
+    formSet = formSetPlatos(queryset = platos)
+
+
+    return render(request,'inicio/home.html',{'menu': formSet})
