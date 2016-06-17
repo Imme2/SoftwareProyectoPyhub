2c530116e18a9bf1d150cd26c94cacfd8b21f7ab
Erick <ericksilvarodriguez@gmail.com>
2016-06-16 21:57:12 -0400

    cambiado el try/except del controlador por un if

--- inicio/controlador.py
+++ inicio/controlador.py
@@ -2,11 +2,11 @@ from Registro.models import menu, item, parametro
 import datetime
 
 def getCurrentMenu():
-	try:
-		parametros = parametro.objects.all()
+	parametros = parametro.objects.all()
+	if len(parametros) == 0:
+		return None
+	else:
 		parametrosActuales = parametro[0]
 		menuActual = parametrosActuales.menuActual
 		platos = menuActual.ofreceRel.all()
 		return platos
-	except:
-		return None
